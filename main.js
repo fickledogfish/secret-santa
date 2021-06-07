c2d17bd6c34a88e2c89478ca90d59bc6f078e7d2
fickledogfish <88333972+fickledogfish@users.noreply.github.com>
2021-06-07 12:38:23 -0300

    Report succeessful registration or error

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const log = require('log');
-require('log-node')();
-const express = require('express');
-const bodyParser = require('body-parser');
+const log = require("log");
+require("log-node")();
+const express = require("express");
+const bodyParser = require("body-parser");
+const url = require("url");
 
 const PORT = process.env.PORT || 1337;
 
@@ -11,27 +12,47 @@ const main = () => {
 	const app = express();
 	const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-	app.get('/', (req, res) => {
+	app.get("/", (req, res) => {
 		res.sendFile(`${__dirname}/client/index.html`)
 	});
 
-	// Register a user
-	app.post('/create', urlencodedParser, (req, res) => {
-		log.notice(req.body);
-		res.send('ok');
+	// Register a user.
+	app.post("/create", urlencodedParser, (req, res) => {
+		// TODO: validate data
+
+		// TODO: check if the user is already in the database
+
+		// TODO: insert user in the database
+
+		const user = req.body;
+		log.notice("Registering %j", user);
+		// res.send('ok');
+
+		// Redirect back into /.
+		res.redirect(url.format({
+			"pathname": "/",
+			"query": {
+				"registered": user.email
+			}
+		}));
 	});
 
-	// Delete a user
-	app.post('/delete', urlencodedParser, (req, res) => {
-		log.notice(req.body);
-		res.send('ok');
+	// Delete a user.
+	app.post("/delete", urlencodedParser, (req, res) => {
+		// TODO: check if a user is in the DB
+
+		// TODO: do not report errors if the user isn't
+
+		log.notice("Requesting deletion");
 	});
 
+	// Run the server.
 	app.listen(PORT, () => {
 		log.notice(`listening at localhost:${PORT}`);
 	});
 };
 
+// Hacking a main function into Node.
 if (require.main === module) {
 	main();
 }
